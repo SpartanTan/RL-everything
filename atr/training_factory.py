f81bd65cd7dc3f42d12752f0c52e2411c9a3804f
zhicun <tanzc9866@126.com>
2023-06-08 17:33:14 +0200

    these files are useless now

--- atr/training_factory.py
+++ atr/training_factory.py
@@ -1,10 +1,33 @@
 import numpy as np
-import scipy.interpolate as interp
 import matplotlib.pyplot as plt
 import time
-from scipy.interpolate import interp1d
-from scipy.interpolate import CubicSpline
-from scipy.integrate import cumtrapz
-from scipy.spatial import cKDTree
-from shapely.geometry import Polygon, Point, LineString
+import pickle
 
+import gymnasium as gym
+from gymnasium import spaces
+
+from env_path import Path
+from RK4 import ATR_RK4
+
+class SimFactoryEnv(gym.Env):
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
+    
+    def __init__(self, dt=0.1):
+        self.path:Path = Path(trajectory_point_interval=0.1,
+                No=8, Nw=8, Lp=6, mu_r=0.25, sigma_d=0.8, shift_distance=1)
+        # take the first waypoint and the first yaw angle as the atr initial state
+        init_state = np.array([self.path.waypoints[0][0], self.path.waypoints[0][1], self.path.yaw_angles[0]])
+        self.atr = ATR_RK4(init_state=init_state, dt=dt)       
+        
+        pass
+    
+    def step(self, action):
+        fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+        self.path.render(True)
+        plt.show()
+    
+    def render(self):
+        pass
+    
+    def reset(self):
+        pass
